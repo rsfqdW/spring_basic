dc1530b74b17ab82ffadefc47ccbbb4185bcefa2
kim <202204266@hufs.ac.kr>
2024-11-29 13:56:50 +0900

    Spring DB Access

--- src/main/java/spring/study1/SpringConfig.java
+++ src/main/java/spring/study1/SpringConfig.java
@@ -1,11 +1,10 @@
 package spring.study1;
 
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import spring.study1.repository.JdbcMemberRepository;
-import spring.study1.repository.MemberRepository;
-import spring.study1.repository.MemoryMemberRepository;
+import spring.study1.repository.*;
 import spring.study1.service.MemberService;
 
 import javax.sql.DataSource;
@@ -13,21 +12,38 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    /*private DataSource dataSource;
 
     @Autowired
     public SpringConfig(DataSource dataSource){
         this.dataSource = dataSource;
+    }*/
+
+    //JPA
+    /*private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em){
+        this.em = em;
+    }*/
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
+    /*@Bean
     public MemberRepository memberRepository(){
         //return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
-    }
+        //return new JdbcMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        //return new JpaMemberRepository(em);
+    }*/
 }
--- src/main/java/spring/study1/domain/Member.java
+++ src/main/java/spring/study1/domain/Member.java
@@ -1,7 +1,14 @@
 package spring.study1.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Member {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
